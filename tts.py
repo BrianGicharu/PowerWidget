4c1f7b6214ae6c1181ca92894e9aa45fa3e88921
blueb33 <57677163+blueb33@users.noreply.github.com>
2021-03-15 03:49:53 +0300

    Update tts.py

--- tts.py
+++ tts.py
@@ -9,17 +9,17 @@ log_out_text = "Logging out the active user"
 op_text = "Operation cancelled"
 
 shutdown_audio = gTTS(text=shutdown_text, lang=lan, slow=False)
-shutdown_audio.save("C:\\Users\\Brian\\IdeaProjects\\PowerWidget\\shutdown_text.wav")
+shutdown_audio.save("shutdown_text.wav")
 
 
 restart_audio = gTTS(text=restart_text, lang=lan, slow=False)
-restart_audio.save("C:\\Users\\Brian\\IdeaProjects\\PowerWidget\\restart_text.wav")
+restart_audio.save("restart_text.wav")
 
 hibernate_audio = gTTS(text=hibernate_text, lang=lan, slow=False)
-hibernate_audio.save("C:\\Users\\Brian\\IdeaProjects\\PowerWidget\\hibernate_text.wav")
+hibernate_audio.save("hibernate_text.wav")
 
 log_out_audio = gTTS(text=log_out_text, lang=lan, slow=False)
-log_out_audio.save("C:\\Users\\Brian\\IdeaProjects\\PowerWidget\\logout_text.wav")
+log_out_audio.save("logout_text.wav")
 
 op_audio = gTTS(text=op_text, lang=lan, slow=True)
-op_audio.save("C:\\Users\\Brian\\IdeaProjects\\PowerWidget\\op_text.wav")
+op_audio.save("op_text.wav")
